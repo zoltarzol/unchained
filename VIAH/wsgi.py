9f0eb0a299d268564d68480d706fd0249f36f44e
zoltarzol <duroisin@gmail.com>
2022-12-02 06:25:53 +0100

    re pushhhhhh

--- VIAH/wsgi.py
+++ VIAH/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unchained.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VIAH.settings')
 
 application = get_wsgi_application()
